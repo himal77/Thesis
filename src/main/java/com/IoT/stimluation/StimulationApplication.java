3d0159dbac5cdad9c1cad52eec2ee4761bffeac4
Himal <purihimal9@gmail.com>
2026-03-22 15:00:59 +0100

    changed the health controller with time stamp

---
+++ src/main/java/com/IoT/stimluation/StimulationApplication.java
@@ -0,0 +1,12 @@
+package com.IoT.stimluation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StimulationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StimulationApplication.class, args);
+	}
+}
